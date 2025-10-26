7e12c9687e88a245679cf57e06827d20266518e9
moy <martinezmoy919@gmail.com>
2025-10-26 15:47:24 -0600

    primer commit - proyecto frontend tiozihuatl

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
      "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        olive: '#96A78D',    // Botones primarios, textos destacados
+        lightgreen: '#B6CEB4', // Enlaces, bordes focus
+        pale: '#D9E9CF',     // Fondos secundarios, gradientes
+        softgray: '#F0F0F0', // Fondos principales, bordes
+      },
+    },
   },
   plugins: [],
 }
