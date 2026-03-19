88637922e8211bd3ef1eff13968c1cb151dca768
moy <169116592+moy2005@users.noreply.github.com>
2026-03-19 04:56:23 -0600

    Update catalog.component.ts

--- src/app/pages/biblioteca/catalogo-bibliografico/catalog.component.ts
+++ src/app/pages/biblioteca/catalogo-bibliografico/catalog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, OnInit, OnDestroy, CUSTOM_ELEMENTS_SCHEMA, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Subject } from 'rxjs';
